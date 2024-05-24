7dd91409782519ddf5834b26faae914fac1c4b72
Daniel J. Bell <daniel@belltech.it>
2024-05-24 14:28:09 +0000

    trap errors, with a message

--- debian12/tests/run_test.sh
+++ debian12/tests/run_test.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+cleanup_function() {
+    result=$?
+    set +x
+    if [ $result -ne 0 ]; then
+       echo "Error occurred in test $test_file"
+       echo "Check logfile \"$test_log\", exit code was \"$result\""
+    else
+       echo "Successful test $test_file"
+    fi
+}    
+
+
 # Check if an argument is provided
 if [ -z "$1" ]; then
     echo "No test name provided. Exiting."
@@ -14,14 +26,17 @@ test_log="logs/test_$1.log"
 if [ ! -f "$test_file" ]; then
     echo "Test \"$1\" does not exist. Exiting."
     exit 1
+
 fi
 
 
 # If an argument is provided, continue with the script
 echo "Running test file: \"$test_file\""
 
-./$test_file > $test_log 2>&1
-result=$?
-echo "Check logfile \"$test_log\", exit code was \"$result\""
+set -xe
+trap 'cleanup_function' EXIT $result
+. ./$test_file > $test_log 2>&1
 
-exit $result
+
+# if we made it here, exit is clean
+exit 0
